8963ae6700ddff2dd2fac0fea26ed696653dc516
thibautschlick <thibaut.schlick@gmail.com>
2021-12-15 20:57:01 -0500

    app

---
+++ App.js
@@ -0,0 +1,68 @@
+import React, { Component, createRef } from 'react'
+import './App.css'
+
+
+import Formulaire from './components/Formulaire'
+import Message from './components/Message'
+
+//Firebase
+import base from './base'
+
+
+
+class App extends Component {
+  state = {
+    messages: {},
+    pseudo: this.props.match.params.pseudo
+  }
+
+  componentDidMount () {
+    base.syncState('/', {
+      context: this,
+      state:'messages'
+    })
+  }
+
+
+
+  addMessage = message => {
+    const messages = { ...this.state.messages }
+
+    messages[`message-${Date.now()}`] = message
+    this.setState({ messages })
+  }
+
+  render () {
+    const messages = Object
+    .keys(this.state.messages)
+    .map(key => (
+      <Message
+      key={key}
+      message={this.state.messages[key].message}
+      pseudo={this.state.messages[key].pseudo}
+      />
+    ))
+
+    console.log(messages)
+    
+
+    return (
+      <div className='box'>
+        <div>
+          <div className='messages'>
+            <div className='message'>
+              { messages }
+            </div>
+           
+          </div>
+        </div>
+        <Formulaire
+          length={140}
+          pseudo={this.state.pseudo}
+          addMessage={this.addMessage} />
+      </div>
+    )
+  }
+}
+
+export default App
